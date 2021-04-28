9b85ca376bffd8e4d6fdfb4e3488013722495a25
k06060212 <k06060212@gmail.com>
2021-04-29 00:12:24 +0900

    환경설정 완료

---
+++ src/main/java/study/Sample01.java
@@ -0,0 +1,11 @@
+package study;
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello, ");
+		System.out.print(" World");
+	}
+
+}
---
+++ src/main/java/study/Sample02.java
@@ -0,0 +1,11 @@
+package study;
+
+public class Sample02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println(", World"); // ln은 line이다.
+	}
+
+}
